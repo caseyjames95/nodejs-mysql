355ad4d895206849abcfbc829c942e6010d52ad6
caseyjames95 <caseypurves@gmail.com>
2019-11-02 18:11:41 -0700

    Setup and layout for node
    sql and node setup

---
+++ bamazon.js
@@ -0,0 +1,13 @@
+// NPM calls
+const mysql = require('mysql2')
+const inquirer = require('inquirer')
+
+// Introduction to Bamazon App
+
+// Display options for app.
+// // See which products are available
+// // Purchase item
+// // // Switch statement
+// // display total
+// // check out
+// // // insufficient quantity should prompt then ask if they would like to return to the main menu
