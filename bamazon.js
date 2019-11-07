40f81d0d387dea9024b284f1e6e5c84494b447e5
caseyjames95 <caseypurves@gmail.com>
2019-11-06 21:13:07 -0800

    update
    added inquirer statements and connection to database.

--- bamazon.js
+++ bamazon.js
@@ -1,13 +1,92 @@
 // NPM calls
 const mysql = require('mysql2')
 const inquirer = require('inquirer')
+const columnify = require('columnify')
 
-// Introduction to Bamazon App
+// Connection to Bamazon App
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'bamazon_db'
+});
 
 // Display options for app.
-// // See which products are available
+let menu = () => {
+    console.log('Welcome to Bamazon!')
+    inquirer.prompt({
+        type: 'list',
+        name: 'choice',
+        message: 'Select an option',
+        choices: ['Purchase item', 'View items']
+    })
+        // // See which products are available
+        .then(({ choice }) => {
+            switch (choice) {
+                case 'Purchase item':
+                    inquirer.prompt({
+                        type: 'input',
+                        name: 'return',
+                        message: ['Which item would you like to purchase?']
+                    })
+                        .then(({ choice1 }) => {
+                            let item;
+                            inquirer.prompt({
+                                type: 'input',
+                                name: 'return',
+                                message: [`what quanity of ${item} would you like to purchase?`],
+                            })
+                                .catch(e => console.log(e))
+                        })
+
+                    break
+
+                case 'View items':
+                    connection.execute(
+                        'SELECT * FROM `products`',
+                        function (err, id) {
+                            console.log(id);
+                            process.exit()
+                        })
+                    inquirer.prompt({
+                        type: 'list',
+                        name: 'return',
+                        message: ['Would you like to return to Main Menu?'],
+                        choices: ['Yes', 'No']
+                    })
+                        .then(({ choice }) => {
+                            switch (choice) {
+                                case 'Yes':
+                                    menu()
+                                    break
+                                case 'No':
+                                    break
+                            }
+                        })
+                        .catch(e => console.log(e))
+                    break
+            }
+        })
+        .catch(e => console.log(e))
+}
+menu()
 // // Purchase item
+let purchase = function () {
+    console.log('purchase')
+}
+
+let view = function () {
+    connection.execute(
+        'SELECT * FROM `products`',
+        function (err, id) {
+            console.log(columnify(id, {columns: ['id', 'product', 'department', 'price', 'stock']}));
+            process.exit()
+        })
+}
+
+
+
+
 // // // Switch statement
 // // display total
 // // check out
-// // // insufficient quantity should prompt then ask if they would like to return to the main menu
